c3c05f64e31136b3a68f96d98dff5f105d8cc230
Omurbek <dseba436@gmail.com>
2025-06-28 22:55:43 +0300

    additional practice
    additional practice

--- studenthub/student/urls.py
+++ studenthub/student/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .views import student, course
-
+from rest_framework.routers import DefaultRouter
+from .views import student, course , StudentListView , CourseListView, StudentCreatortListView, CourseCreatorListView,\
+ CoursetListCreateView, StudentListCreateView, CourseViewSet
 
 urlpatterns = [
     path("student/", student, name="student"),
     path("course/", course, name="course"),
+    path("list/", StudentListView.as_view(), name="StudentListView"),
+    path("courses/list/", CourseListView.as_view(), name="CourseListView"),
+    path("post/", StudentCreatortListView.as_view(), name="StudentCreatortListView"),
+    path("courses/post/", CourseCreatorListView.as_view(), name="CourseCreatorListView"),
+    path("post/course/", CoursetListCreateView.as_view(), name="CoursetListCreateView"),
+    path("post/student/", StudentListCreateView.as_view(), name="StudentListCreateView"),
+    path("post/student/", StudentListCreateView.as_view(), name="StudentListCreateView"),
 ]
+
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+urlpatterns += router.urls
--- studenthub/student/views.py
+++ studenthub/student/views.py
@@ -3,7 +3,17 @@ from .models import Student, Course
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import StudentSerializer, StudentCreatorSerializer, CourseSerializer, CourseCreatorSerializer
+from .serializers import (
+    StudentSerializer,
+    StudentCreatorSerializer,
+    CourseSerializer,
+    CourseCreatorSerializer,
+)
+
+# class based views
+from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.generics import get_object_or_404
 
 
 @api_view(["GET", "POST"])
@@ -47,7 +57,88 @@ def course(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(
-                {"error": f"kurs tabylbady"}, status=status.HTTP_400_BAD_REQUEST)
+                {"error": f"kurs tabylbady"}, status=status.HTTP_400_BAD_REQUEST
+            )
     return Response(
         {"message": f"Hello this is {method} method type"}, status=status.HTTP_200_OK
     )
+
+
+#  class based views
+
+
+class StudentListView(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseListView(ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class StudentCreatortListView(CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentCreatorSerializer
+
+
+class CourseCreatorListView(CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseCreatorSerializer
+
+
+# get list create post
+class CoursetListCreateView(ListCreateAPIView):
+    queryset = Course.objects.all()
+
+    def get_serializer_class(self):
+        get_s = CourseSerializer
+        post_s = CourseCreatorSerializer
+        if self.request.method == "GET":
+            return get_s
+        elif self.request.method == "POST":
+            return post_s
+
+
+class StudentListCreateView(ListCreateAPIView):
+    queryset = Student.objects.all()
+
+    def get_serializer_class(self):
+        get_s = StudentSerializer
+        post_s = StudentCreatorSerializer
+        if self.request.method == "GET":
+            return get_s
+        elif self.request.method == "POST":
+            return post_s
+
+
+# Vieset
+
+
+class CourseViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = Course.objects.all()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Course.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = CourseSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+
+        create_serializer = CourseCreatorSerializer(data=request.data)
+
+        if create_serializer.is_valid():
+            course = create_serializer.save()
+
+            serializer = CourseSerializer(course)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(
+            {"error": create_serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
